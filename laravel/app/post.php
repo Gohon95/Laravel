e20ad28960fda38d5523133a1cdc9d88b1744852
Axel Gonon <gohon95@gmail.com>
2020-02-07 12:33:35 +0100

    Ajout de fichier

---
+++ laravel/app/post.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class post extends Model
+{
+        /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'content',
+    ];
+
+    protected $table = "posts";
+}
